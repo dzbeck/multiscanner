05c7d5cba289763fea4554aefdc576a25052b116
Desiree Beck <dbeck@mitre.org>
2017-12-22 13:46:34 -0500

    changed quote style

--- docs/conf.py
+++ docs/conf.py
@@ -169,10 +169,10 @@ texinfo_documents = [
 from recommonmark.parser import CommonMarkParser
 
 # The suffix of source filenames.
-source_suffix = [’.rst’, ’.md’]
+source_suffix = ['.rst', '.md']
 
 source_parsers = {
-    ’.md’: CommonMarkParser,
+    '.md': CommonMarkParser,
 }
 
 
